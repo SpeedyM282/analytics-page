c30887057f9ec01f0b8b7c038a7fac9655b0e733
speedym282 <abdulaxadrizakulov@gmail.com>
2023-04-26 12:25:45 +0500

    add dropdown

--- src/components/MenuLink/index.jsx
+++ src/components/MenuLink/index.jsx
@@ -1,10 +1,18 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import downArrow from './down-arrow.svg';
 import upArrow from './up-arrow.svg';
 import './style.scss';
 
 const MenuLink = ({links}) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    if(links.subtitles) {
+      setOptions(links.subtitles.map(e => <p key={e} className="menulink__subtitles-option pointer" >{e}</p>))
+    }
+    // eslint-disable-next-line
+  }, [])
 
   const handleClick = () => {
     setDropdownOpen(prev => !prev);
@@ -13,16 +21,17 @@ const MenuLink = ({links}) => {
   return (
     <div className="menulink" >
       <div className="menulink__link" >
-        <span className="menulink__link-title" >{links.title}</span>
+        <p className="menulink__link-title pointer" >{links.title}</p>
         {
           links.subtitles &&
-          <img className="menulink__link-title-arrow" onClick={handleClick} src={dropdownOpen ? upArrow : downArrow} alt="Not Found" />
+          <img className="menulink__link-title-arrow pointer" onClick={handleClick} src={dropdownOpen ? upArrow : downArrow} alt="Not Found" />
         }
       </div>
 
       {
-        links.subtitles &&
+        (links.subtitles && dropdownOpen) &&
         <div className="menulink__subtitles" >
+          {options}
         </div>
       }
     </div>
