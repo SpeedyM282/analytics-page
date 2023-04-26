1eca13a7fcc88af8092c9cb2cd0a8a3da2ece2d0
speedym282 <abdulaxadrizakulov@gmail.com>
2023-04-26 12:33:56 +0500

    add Analytics page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 // import Button from './components/Button';
 // import Input from './components/Input';
 import Sidebar from './components/Sidebar';
+import Analytics from './pages/Analytics';
 import './App.scss';
 
 const App = () => {
   return (
-    <div className="App">
-      {/* <Button/> */}
-      {/* <Input withLink={true} />
-      <Input withLink={false} /> */}
+    <div className="app">
       <Sidebar user='Денис Потемкин' />
+      <main className="app__main">
+        <Analytics />
+      </main>
     </div>
   );
 }
---
+++ src/pages/Analytics/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.scss';
+
+const Analytics = () => {
+  return (
+    <div className="analytics">
+      <h1>Analytics Page</h1>
+    </div>
+  );
+}
+
+export default Analytics;
